87cd1c472688d5fff85813b5a2f7e88d9b06f44a
Robert Keffury <bobby.keffury@me.com>
2020-01-07 14:07:33 -0800

    Created storyboards for login/signUp, onboarding, and main interface.

---
+++ AnywhereFitness1/AnywhereFitness1/View Controllers/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  AnywhereFitness1
+//
+//  Created by Bobby Keffury on 1/7/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //MARK: - Views
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Methods
+
+    
+    
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}
