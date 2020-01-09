b405f4b97fda419a8d173913d8afb17c9526ca8c
Robert Keffury <bobby.keffury@me.com>
2020-01-09 14:45:00 -0800

    Finished Search Functionality.

--- AnywhereFitness1/AnywhereFitness1/View Controllers/Client/ClientSearchDetailViewController.swift
+++ AnywhereFitness1/AnywhereFitness1/View Controllers/Client/ClientSearchDetailViewController.swift
@@ -16,18 +16,48 @@ class ClientSearchDetailViewController: UIViewController {
     
     //MARK: - Outlets
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var durationTextField: UITextField!
+    @IBOutlet weak var intensityTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var currentAthletesTextField: UITextField!
+    @IBOutlet weak var maxAthletesTextField: UITextField!
+    @IBOutlet weak var searchDetailNavBar: UINavigationBar!
+    
     //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateViews()
     }
     
     //MARK: - Methods
     
+    func updateViews() {
+        guard let singleClass = singleClass else { return }
+        
+        searchDetailNavBar.topItem?.title = singleClass.name
+        
+        nameTextField.text = singleClass.name
+        typeTextField.text = singleClass.type
+        startTimeTextField.text = String(describing: singleClass.Start)
+        durationTextField.text = singleClass.Duration
+        intensityTextField.text = singleClass.Intensity
+        locationTextField.text = singleClass.Location
+        currentAthletesTextField.text = String(describing: singleClass.AthleteCount)
+        maxAthletesTextField.text = String(describing: singleClass.MaxAthleteCount)
+    }
+    
     //MARK: - Actions
-
-    //MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "backSegue", sender: self)
+    }
+    
+    @IBAction func joinClassButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "backSegue", sender: self)
     }
 }
